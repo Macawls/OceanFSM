ff3f18779139fd48633a8766d84184e5df8600a8
Macawls <josh@macawls.dev>
2023-02-13 18:11:05 +0200

    refactor: make fields protected, remove fixedUpdate()

--- Runtime/States/State.cs
+++ Runtime/States/State.cs
@@ -4,31 +4,49 @@
     /// The base class for all the states in the FSM.
     /// </summary>
     /// <typeparam name="T">
-    /// A reference type used to associate/bind states and transitions with the state machine.
+    /// A reference type used to associate states and machines with a specific instance of a class.
     /// </typeparam>
     public abstract class State<T> where T : class
     { 
         /// <summary>
         /// The instance used to run the state machine.
         /// </summary>
-        public T Runner { get; private set; }
+        protected T Runner { get; private set; }
 
         /// <summary>
-        /// The machine associated with this state. Will be null if the state machine does not implement <see cref="IAutonomousStateMachine{T}"/>.
+        /// The machine associated with this state. Will be null if the state machine does not implement <see cref="IAutonomousMachine{T}"/>.
         /// </summary>
-        public IAutonomousStateMachine<T> Machine { get; private set; }
+        protected IAutonomousMachine<T> Machine { get; private set; }
         
         internal void InitializeInternal(T runner, IStateMachine<T> machine)
         {
             Runner = runner;
-            Machine = machine as IAutonomousStateMachine<T>;
+            Machine = machine as IAutonomousMachine<T>;
             OnInitialize();
         }
 
+        /// <summary>
+        /// Called when the state machine is built.
+        /// </summary>
         public virtual void OnInitialize() { }
+        
+        /// <summary>
+        /// Called when the state machine enters this state.
+        /// </summary>
         public virtual void OnEnter() { }
+        
+        /// <summary>
+        /// Called when the state machine exits this state.
+        /// </summary>
         public virtual void OnExit() { }
+        
+        
+        /// <summary>
+        /// Called every tick of the state machine.
+        /// </summary>
+        /// <param name="deltaTime">
+        /// The time in seconds since the last frame.
+        /// </param>
         public virtual void OnUpdate(float deltaTime) { }
-        public virtual void OnFixedUpdate(float fixedDeltaTime) { }
     }
 }
